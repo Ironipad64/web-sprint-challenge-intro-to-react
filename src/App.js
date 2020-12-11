4fd4123866481bc0a60af99d9553b43c48635d7e
Ironipad64 <Ironipad64@gmail.com>
2020-12-11 10:58:53 -0800

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import styled, { keyframes } from "styled-components";
 
 
 import axios from "axios";
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -12,27 +14,46 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   const [people, setPeople] = useState([]);
-  const [currentPeopleId, setCurrentPeopleId] = useState("1");
-
+  // const [currentPeopleId, setCurrentPeopleId] = useState("1");
 
 
 
   useEffect(() => {
     axios
-      .get(`https://swapi.dev/api/people/1/`)
+      .get(`https://swapi.dev/api/people/`)
       .then((res) => {
-        debugger;
+        setPeople(res.data);
       })
       .catch((err) => {
-        debugger;
+        console.log(err);
       });
   }, []);
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <StyledHeaders>
+      <div className="App">
+        <h1 className="Header">Star Wars Characters</h1>
+        <h2> Here goes...</h2>
+
+        {/* {people.map((fr) => {
+          return <Character bold />;
+        })} */}
+
+      </div>
+    </StyledHeaders>
   );
 }
 
 export default App;
+
+const kf = keyframes`
+ 100% {
+   opacity: 1;
+   transform: scale(1)
+ }
+`;
+
+const StyledHeaders = styled.div`
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+`
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,29 @@
 // Write your Character component here
 import React, { useState, useEffect } from 'react';
-import axios from "axios";
+// import axios from "axios";
+import styled, { keyframes } from "styled-components";
 
 
-export default function Character() {
+export default function Character({ info, action, bold }) {
+    return (
+        <StyledPeople bold={bold} >
+            {info.name}
+        </StyledPeople>
+    );
+
+
 
 }
+
+const kf = keyframes`
+ 100% {
+   opacity: 1;
+   transform: scale(1)
+ }
+`;
+
+const StyledPeople = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+`
